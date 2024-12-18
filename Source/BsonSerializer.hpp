484b3abfaac824e8b05aac7370cc34295796c48c
AmorZhao <amor_7303@163.com>
2024-12-18 19:41:30 +0800

    deleted standalone test, added Wisent source files, changed cmake list, included static headers

---
+++ Source/BsonSerializer.hpp
@@ -0,0 +1,30 @@
+#include <string>
+namespace bson 
+{
+    namespace serializer 
+    {
+        void* loadAsBson(
+            std::string const& path, 
+            std::string const& sharedMemoryName,
+            std::string const& csvPrefix, 
+            bool disableCsvHandling = false,
+            bool forceReload = false
+        );
+
+        void* loadAsJson(
+            std::string const& path, 
+            std::string const& sharedMemoryName,
+            std::string const& csvPrefix, 
+            bool disableCsvHandling = false,
+            bool forceReload = false
+        );
+
+        void unload(
+            std::string const& sharedMemoryName
+        );
+
+        void free(
+            std::string const& sharedMemoryName
+        );
+    } 
+} 
